4eecb23e67dbf9c80cfbdba47aab3773138c2588
Sheikh sadi <you@example.com>
2022-08-24 07:42:13 -0700

    problem 2 added

---
+++ practise-problem1.js
@@ -0,0 +1,6 @@
+/* Write an arrow function that will take 3 parameters, will multiply
+the parameters and will return the result. */
+
+const multiply = (num1,num2,num3) => num1*num2*num3;
+
+console.log(multiply(2,3,4));
---
+++ practise-problem2.js
@@ -0,0 +1,6 @@
+/* Write the following sentence in three lines and print the result:
+I am a web developer. I love to code. I love to eat biryani. */
+
+console.log(`I am a web developer.
+I have to code. 
+I love to eat biryani`);
