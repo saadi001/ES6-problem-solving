5a2f1b04ba99db4359bb954e15eb5763241e0385
Sheikh sadi <you@example.com>
2022-08-24 08:02:49 -0700

    problem 3 added

---
+++ practise-problem3.js
@@ -0,0 +1,7 @@
+/* Write an arrow function that will take 2 parameters: One parameter
+will come from you and the other parameter will be a default
+parameter. Add these two parameters and return the result. */
+
+const checkingDefault = (a,b=2) =>  a*b;
+
+console.log(checkingDefault(2));
