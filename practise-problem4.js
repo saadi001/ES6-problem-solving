f13acd3c093f1ab81b46d611b1ea145f6b1f29ad
Sheikh sadi <you@example.com>
2022-08-24 10:39:57 -0700

    problem 4 added

--- practise-problem4.js
+++ practise-problem4.js
@@ -5,3 +5,17 @@ b) Check if the length of each element is even, push elements
 with even length to a new array and return the result
 Print the result. */
 
+const evenLength = array => {
+    [2,4,5]
+};
+
+console.log(evenLength);
+
+const materials = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+  
+  console.log(materials.map(material => material.length));
