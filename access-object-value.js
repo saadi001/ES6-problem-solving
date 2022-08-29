b5c67b6f3c966de42d661bdab0e0907614289f47
Sheikh sadi <you@example.com>
2022-08-29 01:14:09 -0700

    another commit

---
+++ access-object-value.js
@@ -0,0 +1,22 @@
+// console the value of age 
+const student = {
+    name: 'fredie',
+    age: 27
+}
+console.log(student.age);
+
+
+// problem 2: console the value of city
+let data = {
+    location: [
+        {
+            latitude: '34.5 , 37.8',
+            longitude: '98.77 , 58.7',
+            city: 'dhaka',
+            country: 'bangladesh'
+
+        }
+    ]
+}
+
+console.log(data.location[0].city);
